881c6eed6bb03eabd23a1ca54d7c191431a507c5
Durbadala <durbadalapadhan2004@gmail.com>
2023-07-22 07:01:31 +0530

    design upgrade

--- lib/home/widgets/events_card.dart
+++ lib/home/widgets/events_card.dart
@@ -10,95 +10,95 @@ class EventsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: MediaQuery.of(context).size.width-50,
       height: 320,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
+      decoration: BoxDecoration(
+        borderRadius:BorderRadius.circular(12),
+        border: Border.all(
+          color: Colors.grey,
+          width: 1,
         ),
-        color: Colors.white,
-        elevation: 10,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width-50,
-              height: 150,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-                  color: Color(0xffdde3c2)
-              ),
-              child:ClipRRect(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
-                child: Image.asset(event.photo,
-                  fit: BoxFit.fitWidth,
-                ),
+      ),
+      child:Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width-50,
+            height: 150,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+                color: Color(0xffdde3c2)
+            ),
+            child:ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+              child: Image.asset(event.photo,
+                fit: BoxFit.fitWidth,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    event.eventsName,
-                    style:GoogleFonts.inter(
-                      color: const Color(0xFF598BED),
-                      fontSize: 15,
-                      fontWeight: FontWeight.w700,
-                    ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  event.eventsName,
+                  style:GoogleFonts.inter(
+                    color: const Color(0xFF598BED),
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
                   ),
-                  const SizedBox(height: 5,),
-                  Text(
-                    event.eventsDesc,
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
+                ),
+                const SizedBox(height: 5,),
+                Text(
+                  event.eventsDesc,
+                  style: GoogleFonts.inter(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
                   ),
-                  const SizedBox(height: 10,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        event.date,
-                        style:GoogleFonts.inter(
-                          color: const Color(0xFF6C747A),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
+                ),
+                const SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      event.date,
+                      style:GoogleFonts.inter(
+                        color: const Color(0xFF6C747A),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          color: const Color(0xff598bed),
+                          width: 2,
                         ),
                       ),
-                      Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                            border: Border.all(
-                              color: const Color(0xff598bed),
-                              width: 2,
-                            ),
-                        ),
-                        child: const Center(
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal:18),
-                            child: Text('Book',
-                              style: TextStyle(
+                      child: const Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal:18),
+                          child: Text('Book',
+                            style: TextStyle(
                               fontSize: 20,
                               color: Color(0xff598bed),
                             ),
 
-                            ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/home/widgets/lesson_card.dart
+++ lib/home/widgets/lesson_card.dart
@@ -8,74 +8,74 @@ class LessonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: MediaQuery.of(context).size.width-50,
       height: 310,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
+      decoration: BoxDecoration(
+        borderRadius:BorderRadius.circular(12),
+        border: Border.all(
+          color: Colors.grey,
+          width: 1,
         ),
-        color: Colors.white,
-        elevation: 10,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width-50,
-              height: 150,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
-                  color: Color(0xffdde3c2)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width-50,
+            height: 150,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+                color: Color(0xffdde3c2)
+            ),
+            child:ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+              child: Image.asset(lesson.photo,
+                fit: BoxFit.fitWidth,
               ),
-              child:ClipRRect(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
-                child: Image.asset(lesson.photo,
-                  fit: BoxFit.fitWidth,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  lesson.lessonName,
+                  style:GoogleFonts.inter(
+                    color: const Color(0xFF598BED),
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    lesson.lessonName,
-                    style:GoogleFonts.inter(
-                      color: const Color(0xFF598BED),
-                      fontSize: 15,
-                      fontWeight: FontWeight.w700,
-                    ),
+                const SizedBox(height: 5,),
+                Text(
+                  lesson.lessonDesc,
+                  style: GoogleFonts.inter(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
                   ),
-                  const SizedBox(height: 5,),
-                  Text(
-                    lesson.lessonDesc,
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${lesson.duration} min',
-                        style:GoogleFonts.inter(
-                          color: const Color(0xFF6C747A),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
+                ),
+                const SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '${lesson.duration} min',
+                      style:GoogleFonts.inter(
+                        color: const Color(0xFF6C747A),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
                       ),
-                      const Icon(Icons.lock_outline)
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                    const Icon(Icons.lock_outline)
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
